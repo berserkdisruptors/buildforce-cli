72a99e5df8e1b2727204c5ab75ce67c4d073bd1d
Nichita Sindie <nichita.sindie@gmail.com>
2025-11-06 19:34:53 +0200

    feat: add conventional commits enforcement

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation
+        'style',    // Formatting
+        'refactor', // Code restructuring
+        'perf',     // Performance
+        'test',     // Tests
+        'build',    // Build system
+        'ci',       // CI/CD
+        'chore',    // Maintenance
+        'revert'    // Revert commit
+      ]
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100]
+  }
+};
